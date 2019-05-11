f146a8e147738494ed73a16be58ec094179e2e04
Karsten Gohm <kasn@mac.com>
2019-05-11 09:14:19 +0200

    add all known numbers, fix reset

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,57 @@
 <script>
   import range from "lodash/range";
-  import lights from "./configurations.js";
+  import configuration from "./configurations.js";
 
   const numbers = range(1, 13);
 
-  let res = null;
-
   const emptySelection = {
-    c1: [null, null],
-    c2: [null, null]
+    console1: [null, null],
+    console2: [null, null]
   };
 
   let selection = emptySelection;
 
-  let possibleOptions = [];
+  function filterOptions(selection) {
+    let possibleOptions = configuration;
+
+    if (selection.console1[0] !== null) {
+      possibleOptions = possibleOptions.filter(light => {
+        return light[`console1`][0] === selection.console1[0];
+      });
+    }
+
+    if (selection.console1[1] !== null) {
+      possibleOptions = possibleOptions.filter(light => {
+        return light[`console1`][1] === selection.console1[1];
+      });
+    }
+
+    if (selection.console2[0] !== null) {
+      possibleOptions = possibleOptions.filter(light => {
+        return light[`console2`][0] === selection.console2[0];
+      });
+    }
+
+    if (selection.console2[1] !== null) {
+      possibleOptions = possibleOptions.filter(light => {
+        return light[`console2`][1] === selection.console2[1];
+      });
+    }
+
+    return possibleOptions;
+  }
+
+  $: possibleOptions = filterOptions(selection);
 
   function consoleClick(consoleNum, consoleRow, num) {
-    selection[`c${consoleNum}`][consoleRow] = num;
-
-    if (selection.c1[0] !== null) {
-      possibleOptions = lights.filter(light => {
-        return light[`console1`][0] === selection.c1[0];
-      });
-    }
-
-    if (selection.c1[1] !== null) {
-      possibleOptions = lights.filter(light => {
-        return light[`console1`][1] === selection.c1[1];
-      });
-    }
-
-    if (selection.c2[0] !== null) {
-      possibleOptions = lights.filter(light => {
-        return light[`console2`][0] === selection.c2[0];
-      });
-    }
-
-    if (selection.c2[1] !== null) {
-      possibleOptions = lights.filter(light => {
-        return light[`console2`][1] === selection.c2[1];
-      });
-    }
+    selection[consoleNum][consoleRow] = num;
   }
 
   function reset() {
-    selection = emptySelection;
+    selection = {
+      console1: [null, null],
+      console2: [null, null]
+    };
     possibleOptions = [];
   }
 </script>
@@ -76,19 +83,19 @@
 <div class="console">
   <h2>console 1</h2>
   {#each numbers as num}
-    <button class:selected={selection[`c1`][0] === { num }} on:click={() => consoleClick(1, 0, num)}>{num}</button>
+    <button class:selected={selection[`console1`][0] === num} on:click={() => consoleClick('console1', 0, num)}>{num}</button>
   {/each}
   <hr />
   {#each numbers as num}
-    <button class:selected={selection[`c1`][1] === { num }} on:click={() => consoleClick(1, 1, num)}>{num}</button>
+    <button class:selected={selection[`console1`][1] === num} on:click={() => consoleClick('console1', 1, num)}>{num}</button>
   {/each}
   <h2>console 2</h2>
   {#each numbers as num}
-    <button class:selected={selection[`c2`][0] === { num }} on:click={() => consoleClick(2, 0, num)}>{num}</button>
+    <button class:selected={selection[`console2`][0] === num} on:click={() => consoleClick('console2', 0, num)}>{num}</button>
   {/each}
   <hr />
   {#each numbers as num}
-    <button class:selected={selection[`c2`][1] === { num }} on:click={() => consoleClick(2, 1, num)}>{num}</button>
+    <button class:selected={selection[`console2`][1] === num} on:click={() => consoleClick('console2', 1, num)}>{num}</button>
   {/each}
 
   {#each possibleOptions as option}
--- src/configurations.js
+++ src/configurations.js
@@ -54,6 +54,393 @@ const lights = [
       color: "yellow",
       number: 2
     }
+  },
+
+  {
+    console1: [2, 6],
+    console2: [5, 4],
+    node: {
+      color: "cyan",
+      number: 1
+    }
+  },
+
+  {
+    console1: [2, 7],
+    console2: [2, 11],
+    node: {
+      color: "yellow",
+      number: 6
+    }
+  },
+
+  {
+    console1: [2, 8],
+    console2: [12, 3],
+    node: {
+      color: "purple",
+      number: 5
+    }
+  },
+
+  {
+    console1: [3, 3],
+    console2: [1, 3],
+    node: {
+      color: "green",
+      number: 7
+    }
+  },
+
+  {
+    console1: [4, 3],
+    console2: [2, 4],
+    node: {
+      color: "green",
+      number: 1
+    }
+  },
+
+  {
+    console1: [4, 3],
+    console2: [2, 11],
+    node: {
+      color: "purple",
+      number: 6
+    }
+  },
+
+  {
+    console1: [4, 6],
+    console2: [5, 6],
+    node: {
+      color: "blue",
+      number: 4
+    }
+  },
+
+  {
+    console1: [4, 8],
+    console2: [12, 8],
+    node: {
+      color: "white",
+      number: 5
+    }
+  },
+
+  {
+    console1: [4, 9],
+    console2: [9, 4],
+    node: {
+      color: "yellow",
+      number: 1
+    }
+  },
+
+  {
+    console1: [5, 6],
+    console2: [7, 3],
+    node: {
+      color: "red",
+      number: 5
+    }
+  },
+
+  {
+    console1: [5, 9],
+    console2: [11, 7],
+    node: {
+      color: "purple",
+      number: 7
+    }
+  },
+
+  {
+    console1: [6, 5],
+    console2: [6, 10],
+    node: {
+      color: "purple",
+      number: 1
+    }
+  },
+
+  {
+    console1: [6, 9],
+    console2: [12, 10],
+    node: {
+      color: "white",
+      number: 7
+    }
+  },
+
+  {
+    console1: [6, 11],
+    console2: [11, 4],
+    node: {
+      color: "cyan",
+      number: 4
+    }
+  },
+
+  {
+    console1: [7, 2],
+    console2: [8, 3],
+    node: {
+      color: "white",
+      number: 6
+    }
+  },
+
+  {
+    console1: [6, 11],
+    console2: [11, 4],
+    node: {
+      color: "cyan",
+      number: 4
+    }
+  },
+
+  {
+    console1: [7, 2],
+    console2: [8, 3],
+    node: {
+      color: "white",
+      number: 6
+    }
+  },
+
+  {
+    console1: [7, 4],
+    console2: [2, 7],
+    node: {
+      color: "green",
+      number: 5
+    }
+  },
+
+  {
+    console1: [7, 9],
+    console2: [6, 5],
+    node: {
+      color: "yellow",
+      number: 4
+    }
+  },
+
+  {
+    console1: [7, 12],
+    console2: [1, 2],
+    node: {
+      color: "blue",
+      number: 5
+    }
+  },
+
+  {
+    console1: [8, 4],
+    console2: [5, 8],
+    node: {
+      color: "cyan",
+      number: 2
+    }
+  },
+
+  {
+    console1: [8, 5],
+    console2: [11, 8],
+    node: {
+      color: "cyan",
+      number: 7
+    }
+  },
+
+  {
+    console1: [8, 6],
+    console2: [2, 9],
+    node: {
+      color: "white",
+      number: 3
+    }
+  },
+
+  {
+    console1: [8, 6],
+    console2: [9, 2],
+    node: {
+      color: "yellow",
+      number: 5
+    }
+  },
+
+  {
+    console1: [8, 10],
+    console2: [5, 6],
+    node: {
+      color: "red",
+      number: 3
+    }
+  },
+
+  {
+    console1: [8, 12],
+    console2: [5, 9],
+    node: {
+      color: "cyan",
+      number: 6
+    }
+  },
+
+  {
+    console1: [9, 3],
+    console2: [12, 7],
+    node: {
+      color: "green",
+      number: 3
+    }
+  },
+
+  {
+    console1: [9, 7],
+    console2: [7, 8],
+    node: {
+      color: "purple",
+      number: 4
+    }
+  },
+
+  {
+    console1: [9, 9],
+    console2: [8, 10],
+    node: {
+      color: "purple",
+      number: 2
+    }
+  },
+
+  {
+    console1: [10, 2],
+    console2: [3, 8],
+    node: {
+      color: "white",
+      number: 2
+    }
+  },
+
+  {
+    console1: [10, 3],
+    console2: [2, 3],
+    node: {
+      color: "green",
+      number: 4
+    }
+  },
+
+  {
+    console1: [10, 5],
+    console2: [6, 2],
+    node: {
+      color: "blue",
+      number: 2
+    }
+  },
+
+  {
+    console1: [10, 12],
+    console2: [11, 5],
+    node: {
+      color: "green",
+      number: 6
+    }
+  },
+
+  {
+    console1: [11, 1],
+    console2: [5, 7],
+    node: {
+      color: "blue",
+      number: 6
+    }
+  },
+
+  {
+    console1: [11, 4],
+    console2: [2, 8],
+    node: {
+      color: "blue",
+      number: 7
+    }
+  },
+
+  {
+    console1: [11, 4],
+    console2: [4, 11],
+    node: {
+      color: "yellow",
+      number: 7
+    }
+  },
+
+  {
+    console1: [11, 4],
+    console2: [5, 4],
+    node: {
+      color: "red",
+      number: 4
+    }
+  },
+
+  {
+    console1: [11, 6],
+    console2: [12, 8],
+    node: {
+      color: "red",
+      number: 7
+    }
+  },
+
+  {
+    console1: [11, 7],
+    console2: [6, 3],
+    node: {
+      color: "purple",
+      number: 3
+    }
+  },
+
+  {
+    console1: [11, 11],
+    console2: [7, 3],
+    node: {
+      color: "blue",
+      number: 3
+    }
+  },
+
+  {
+    console1: [12, 2],
+    console2: [11, 4],
+    node: {
+      color: "cyan",
+      number: 5
+    }
+  },
+
+  {
+    console1: [12, 5],
+    console2: [7, 1],
+    node: {
+      color: "white",
+      number: 4
+    }
+  },
+
+  {
+    console1: [12, 5],
+    console2: [12, 11],
+    node: {
+      color: "blue",
+      number: 1
+    }
   }
 ];
 
