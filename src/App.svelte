3e9848c185c5abc7fa1a539727c60dfbae958fe4
Karsten Gohm <kasn@mac.com>
2019-05-15 17:31:17 +0200

    add arc config, add siva style

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
   import range from "lodash/range";
-  import configuration from "./configurations.js";
+  import configurations from "./configurations.js";
+  import Gh from "./Components/Gh.svelte";
+  import Arc from "./Components/Elements/Arc.svelte";
+  import Solar from "./Components/Elements/Solar.svelte";
+  import Void from "./Components/Elements/Void.svelte";
+  import Siva from "./Components/Siva.svelte";
 
   const numbers = range(1, 13);
 
@@ -10,9 +15,16 @@
   };
 
   let selection = emptySelection;
+  $: activeConfiguration = "arc"; // @TODO get week an preselect
+
+  function setActiveConfiguration(element) {
+    activeConfiguration = element;
+    possibleOptions = [];
+    reset();
+  }
 
   function filterOptions(selection) {
-    let possibleOptions = configuration;
+    let possibleOptions = configurations[activeConfiguration].configuration;
 
     if (selection.console1[0] !== null) {
       possibleOptions = possibleOptions.filter(light => {
@@ -54,20 +66,36 @@
     };
     possibleOptions = [];
   }
+
+  console.log(selection);
 </script>
 
 <style>
   h1 {
-    color: purple;
+    color: red;
   }
 
   h1 button {
     font-size: 12px;
   }
 
+  button {
+    background-color: black;
+    color: red;
+    border: 0;
+  }
+
+  button.reset {
+    font-size: 30px;
+  }
+
   .console button {
-    width: 25%;
+    width: 24%;
     font-size: 25px;
+    color: red;
+    background-color: black;
+    border: 2px solid red;
+    margin: 1px;
   }
 
   .console button.selected {
@@ -83,76 +111,42 @@
     margin: 5px;
     width: 50%;
   }
-
-  .github-corner:hover .octo-arm {
-    animation: octocat-wave 560ms ease-in-out;
-  }
-  @keyframes octocat-wave {
-    0%,
-    100% {
-      transform: rotate(0);
-    }
-    20%,
-    60% {
-      transform: rotate(-25deg);
-    }
-    40%,
-    80% {
-      transform: rotate(10deg);
-    }
-  }
-  @media (max-width: 500px) {
-    .github-corner:hover .octo-arm {
-      animation: none;
-    }
-    .github-corner .octo-arm {
-      animation: octocat-wave 560ms ease-in-out;
-    }
-  }
 </style>
 
-<a
-  href="https://github.com/kasn/zero-hour"
-  class="github-corner"
-  aria-label="View source on GitHub"
-  target="_blank">
-  <svg
-    width="80"
-    height="80"
-    viewBox="0 0 250 250"
-    style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0;
-    right: 0;"
-    aria-hidden="true">
-    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
-    <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7
-      120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9
-      125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin:
-      130px 106px;" class="octo-arm" />
-    <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6
-      C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0
-      C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1
-      C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4
-      C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9
-      C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5
-      C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9
-      L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body" />
-  </svg>
-</a>
+<Gh />
+
 <h1>
-  Zero Hour
-  <button on:click={reset}>reset</button>
+  <button on:click={() => setActiveConfiguration('void')}>
+    <Void />
+  </button>
+  <button on:click={() => setActiveConfiguration('arc')}>
+    <Arc />
+  </button>
+  <button on:click={() => setActiveConfiguration('solr')}>
+    <Solar />
+  </button>
+  <button class="reset" on:click={reset}>reset</button>
 </h1>
+<h1>{configurations[activeConfiguration].name}</h1>
 <div class="console">
   <h2>console 1</h2>
   <div class="wrapper">
     <div class="side">
       {#each numbers as num}
-        <button class:selected={selection[`console1`][0] === num} on:click={() => consoleClick('console1', 0, num)}>{num}</button>
+        <button
+          class:selected={selection[`console1`][0] === num}
+          on:click={() => consoleClick('console1', 0, num)}>
+           {num}
+        </button>
       {/each}
     </div>
     <div class="side">
       {#each numbers as num}
-        <button class:selected={selection[`console1`][1] === num} on:click={() => consoleClick('console1', 1, num)}>{num}</button>
+        <button
+          class:selected={selection[`console1`][1] === num}
+          on:click={() => consoleClick('console1', 1, num)}>
+           {num}
+        </button>
       {/each}
     </div>
   </div>
@@ -160,12 +154,20 @@
   <div class="wrapper">
     <div class="side">
       {#each numbers as num}
-        <button class:selected={selection[`console2`][0] === num} on:click={() => consoleClick('console2', 0, num)}>{num}</button>
+        <button
+          class:selected={selection[`console2`][0] === num}
+          on:click={() => consoleClick('console2', 0, num)}>
+           {num}
+        </button>
       {/each}
     </div>
     <div class="side">
       {#each numbers as num}
-        <button class:selected={selection[`console2`][1] === num} on:click={() => consoleClick('console2', 1, num)}>{num}</button>
+        <button
+          class:selected={selection[`console2`][1] === num}
+          on:click={() => consoleClick('console2', 1, num)}>
+           {num}
+        </button>
       {/each}
     </div>
   </div>
--- src/configurations.js
+++ src/configurations.js
@@ -1,4 +1,23 @@
-const lights = [
+const colors = {
+  red: "red",
+  green: "green",
+  purple: "purple",
+  cyan: "cyan",
+  white: "white",
+  blue: "blue",
+  yellow: "yellow"
+};
+
+// {
+//   console1: [, ],
+//   console2: [null, null],
+//   node: {
+//     color: colors.,
+//     number:
+//   }
+// },
+
+const voidConfiguration = [
   {
     console1: [1, 1],
     console2: [2, 12],
@@ -444,4 +463,407 @@ const lights = [
   }
 ];
 
-export default lights;
+const arcConfiguration = [
+  {
+    console1: [1, 4],
+    console2: [null, null],
+    node: {
+      color: colors.red,
+      number: 6
+    }
+  },
+  {
+    console1: [1, 6],
+    console2: [null, null],
+    node: {
+      color: colors.yellow,
+      number: 7
+    }
+  },
+  {
+    console1: [2, 4],
+    console2: [null, null],
+    node: {
+      color: colors.green,
+      number: 3
+    }
+  },
+  {
+    console1: [2, 9],
+    console2: [4, 3],
+    node: {
+      color: colors.green,
+      number: 2
+    }
+  },
+  {
+    console1: [2, 9],
+    console2: [5, 9],
+    node: {
+      color: colors.purple,
+      number: 6
+    }
+  },
+  {
+    console1: [3, 5],
+    console2: [null, null],
+    node: {
+      color: colors.cyan,
+      number: 2
+    }
+  },
+  {
+    console1: [3, 8],
+    console2: [,],
+    node: {
+      color: colors.yellow,
+      number: 4
+    }
+  },
+  {
+    console1: [4, 4],
+    console2: [,],
+    node: {
+      color: colors.cyan,
+      number: 1
+    }
+  },
+  {
+    console1: [4, 5],
+    console2: [null, null],
+    node: {
+      color: colors.white,
+      number: 4
+    }
+  },
+  {
+    console1: [4, 6],
+    console2: [null, null],
+    node: {
+      color: colors.purple,
+      number: 2
+    }
+  },
+  {
+    console1: [4, 7],
+    console2: [null, null],
+    node: {
+      color: colors.blue,
+      number: 5
+    }
+  },
+  {
+    console1: [4, 8],
+    console2: [null, null],
+    node: {
+      color: colors.white,
+      number: 5
+    }
+  },
+  {
+    console1: [4, 9],
+    console2: [null, null],
+    node: {
+      color: colors.cyan,
+      number: 5
+    }
+  },
+  {
+    console1: [4, 12],
+    console2: [2, 7],
+    node: {
+      color: colors.green,
+      number: 6
+    }
+  },
+  {
+    console1: [4, 12],
+    console2: [5, 6],
+    node: {
+      color: colors.yellow,
+      number: 1
+    }
+  },
+  {
+    console1: [5, 4],
+    console2: [5, 2],
+    node: {
+      color: colors.red,
+      number: 5
+    }
+  },
+  {
+    console1: [5, 4],
+    console2: [11, 8],
+    node: {
+      color: colors.cyan,
+      number: 7
+    }
+  },
+  {
+    console1: [5, 8],
+    console2: [null, null],
+    node: {
+      color: colors.red,
+      number: 7
+    }
+  },
+  {
+    console1: [5, 9],
+    console2: [1, 1],
+    node: {
+      color: colors.red,
+      number: 1
+    }
+  },
+  {
+    console1: [5, 9],
+    console2: [6, 8],
+    node: {
+      color: colors.red,
+      number: 4
+    }
+  },
+  {
+    console1: [6, 2],
+    console2: [1, 7],
+    node: {
+      color: colors.purple,
+      number: 5
+    }
+  },
+  {
+    console1: [6, 2],
+    console2: [12, 5],
+    node: {
+      color: colors.blue,
+      number: 3
+    }
+  },
+  {
+    console1: [6, 3],
+    console2: [null, null],
+    node: {
+      color: colors.white,
+      number: 1
+    }
+  },
+  {
+    console1: [6, 5],
+    console2: [null, null],
+    node: {
+      color: colors.blue,
+      number: 1
+    }
+  },
+  {
+    console1: [7, 3],
+    console2: [null, null],
+    node: {
+      color: colors.cyan,
+      number: 3
+    }
+  },
+  {
+    console1: [7, 9],
+    console2: [null, null],
+    node: {
+      color: colors.yellow,
+      number: 5
+    }
+  },
+  {
+    console1: [8, 1],
+    console2: [null, null],
+    node: {
+      color: colors.red,
+      number: 2
+    }
+  },
+  {
+    console1: [8, 9],
+    console2: [null, null],
+    node: {
+      color: colors.green,
+      number: 7
+    }
+  },
+  {
+    console1: [8, 11],
+    console2: [null, null],
+    node: {
+      color: colors.cyan,
+      number: 4
+    }
+  },
+  {
+    console1: [8, 12],
+    console2: [null, null],
+    node: {
+      color: colors.white,
+      number: 7
+    }
+  },
+  {
+    console1: [9, 1],
+    console2: [null, null],
+    node: {
+      color: colors.yellow,
+      number: 3
+    }
+  },
+  {
+    console1: [9, 6],
+    console2: [null, null],
+    node: {
+      color: colors.purple,
+      number: 4
+    }
+  },
+  {
+    console1: [9, 10],
+    console2: [null, null],
+    node: {
+      color: colors.yellow,
+      number: 2
+    }
+  },
+  {
+    console1: [9, 11],
+    console2: [1, 3],
+    node: {
+      color: colors.blue,
+      number: 7
+    }
+  },
+  {
+    console1: [9, 11],
+    console2: [12, 6],
+    node: {
+      color: colors.blue,
+      number: 4
+    }
+  },
+  {
+    console1: [9, 12],
+    console2: [null, null],
+    node: {
+      color: colors.purple,
+      number: 1
+    }
+  },
+  {
+    console1: [10, 8],
+    console2: [null, null],
+    node: {
+      color: colors.red,
+      number: 3
+    }
+  },
+  {
+    console1: [10, 9],
+    console2: [null, null],
+    node: {
+      color: colors.blue,
+      number: 6
+    }
+  },
+  {
+    console1: [10, 11],
+    console2: [3, 2],
+    node: {
+      color: colors.green,
+      number: 1
+    }
+  },
+  {
+    console1: [10, 11],
+    console2: [9, 3],
+    node: {
+      color: colors.yellow,
+      number: 6
+    }
+  },
+  {
+    console1: [10, 12],
+    console2: [8, 4],
+    node: {
+      color: colors.green,
+      number: 5
+    }
+  },
+  {
+    console1: [10, 12],
+    console2: [9, 6],
+    node: {
+      color: colors.white,
+      number: 3
+    }
+  },
+  {
+    console1: [11, 1],
+    console2: [null, null],
+    node: {
+      color: colors.white,
+      number: 2
+    }
+  },
+  {
+    console1: [11, 11],
+    console2: [null, null],
+    node: {
+      color: colors.white,
+      number: 6
+    }
+  },
+  {
+    console1: [15, 5],
+    console2: [null, null],
+    node: {
+      color: colors.cyan,
+      number: 6
+    }
+  },
+  {
+    console1: [12, 7],
+    console2: [null, null],
+    node: {
+      color: colors.purple,
+      number: 3
+    }
+  },
+  {
+    console1: [12, 8],
+    console2: [null, null],
+    node: {
+      color: colors.blue,
+      number: 2
+    }
+  },
+  {
+    console1: [12, 12],
+    console2: [null, null],
+    node: {
+      color: colors.green,
+      number: 4
+    }
+  }
+];
+
+const solorConfiguration = [];
+const configuration = {
+  void: {
+    name: "Void",
+    configuration: voidConfiguration
+  },
+  solr: {
+    name: "Solr",
+    configuration: solorConfiguration
+  },
+  arc: {
+    name: "Arc",
+    configuration: arcConfiguration
+  }
+};
+
+export default configuration;
